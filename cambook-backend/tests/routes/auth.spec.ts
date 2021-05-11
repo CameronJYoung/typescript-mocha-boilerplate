e19d25faeffdfe171d868caad598a3d984eb724b
CameronJYoung <cameronjyoung123@gmail.com>
2021-05-12 00:23:31 +0100

    Added route test to check it accepts json data

--- cambook-backend/tests/routes/auth.spec.ts
+++ cambook-backend/tests/routes/auth.spec.ts
@@ -7,18 +7,40 @@ describe('Auth Router Tests', () => {
 
 	describe('Register Route Tests', () => {
 		it("register route exists", (done) => {
-			request.post('/api/auth/register').end((err, res: supertest.Response) => {
-				if (err) {
-					console.log(err);
-				}
-	
-				expect(res.statusCode).to.be.equal(200);
-				done();
-			})
+			request.post('/api/auth/register')
+				.end((err, res: supertest.Response) => {
+					if (err) {
+						console.log(err);
+					}
+		
+					expect(res.statusCode).to.be.equal(200);
+					done();
+				})
+		});
+
+		it("register route accepts json data", (done) => {
+			request.post('/api/auth/register')
+				.send({
+					"username": "cameron",
+					"password": "password",
+					"confirmPassword": "password"
+				})
+				.set('Content-Type', 'application/json')
+				.set('Accept', 'application/json')
+				.expect(200)
+				.end((err, res: supertest.Response) => {
+					if (err) {
+						console.log(err);
+					}
+		
+					expect(res.statusCode).to.be.equal(200);
+					done();
+				})
 		});
 	})
 
+
+
+
 	
-
-
 });
