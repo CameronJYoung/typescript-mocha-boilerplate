cb5c0000c68c412cd364acc4fb25b339064a763f
CameronJYoung <cameronjyoung123@gmail.com>
2021-05-12 01:46:20 +0100

    Started adding tests for database config checks

---
+++ cambook-backend/tests/database/config.spec.ts
@@ -0,0 +1,18 @@
+//import { expect } from 'chai';
+
+import connection from '../../src/config/db.connection'
+
+describe('db config checks', () => {
+
+	describe('Register Route Tests', () => {
+		it('connects to db successfully', (done) => {
+			
+		});
+		it('db can close successfully', (done) => {
+			
+		});
+
+
+		
+	});
+});
