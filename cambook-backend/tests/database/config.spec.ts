957f1dd8dd91d10b8c37dfa215d548d6676401ad
CameronJYoung <cameronjyoung123@gmail.com>
2021-05-13 01:42:47 +0100

    added db config test

--- cambook-backend/tests/database/config.spec.ts
+++ cambook-backend/tests/database/config.spec.ts
@@ -3,6 +3,9 @@
 import connection from '../../src/config/db.connection' // typeORM for the database
 
 describe('db config checks', () => {
+	it('config has correct information', (done) => {
+
+	});
 	it('connects to db successfully', (done) => {
 
 	});
